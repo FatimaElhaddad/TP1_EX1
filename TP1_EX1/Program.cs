02fd1845d5cafce9b2c773b8b4be33d54907d951
FatimaELhaddad <fatima.elhaddad3@usmba.ac.ma>
2022-11-01 17:27:11 +0100

    Ajoutez des fichiers projet.

---
+++ TP1_EX1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Entrer un nbr");
+            n = int.Parse(Console.ReadLine());
+            is_prime(n);
+        }
+        public static void is_prime(int n)
+        {
+            int prime = 1;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    prime = 0;
+                    break;
+                }
+
+            }
+            if (prime == 0)
+                Console.WriteLine("Le nbr n'est pas premier");
+
+            else
+                Console.WriteLine("Le nbr est premier");
+            Console.ReadKey();
+        }
+    }
+}
